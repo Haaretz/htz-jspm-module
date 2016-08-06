d16351f6105299dff63d3493e43ca9c965b003c4
TxHawks <tofu.hawks@gmail.com>
2016-08-06 12:29:09 +0300

    update dependencies

--- boilerplate/jspm.config.js
+++ boilerplate/jspm.config.js
@@ -1,4 +1,21 @@
 SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/",
+    "github:": "jspm_packages/github/",
+    "mouse/": "js/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.13",
+      "babel-runtime": "npm:babel-runtime@5.8.38",
+      "babel": "npm:babel-core@5.8.38",
+      "core-js": "npm:core-js@1.2.7",
+      "path": "github:jspm/nodelibs-path@0.2.0-alpha"
+    }
+  },
   transpiler: "plugin-babel",
   packages: {
     "<%= moduleSafeName %>": {
@@ -18,8 +35,6 @@ SystemJS.config({
     "npm:*.json",
     "github:*/*.json"
   ],
-  map: {
-    "plugin-babel": "npm:systemjs-plugin-babel@0.0.12"
-  },
+  map: {},
   packages: {}
 });
